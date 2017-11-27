67cbac075917ed8339172f004ff7673ce2f0ac76
farkaszsolt <zsolt.farkas@mechatromotive.com>
2017-11-27 13:09:28 +0100

    feat cica

--- STM32Cube_FW_F7_V1.8.0/Projects/STM32746G-Discovery/GreenFox/led_blinker/Src/main.cpp
+++ STM32Cube_FW_F7_V1.8.0/Projects/STM32746G-Discovery/GreenFox/led_blinker/Src/main.cpp
@@ -1,13 +1,4 @@
- /**
-  ******************************************************************************
-  * @file    Templates/Src/main.c 
-  * @author  MCD Application Team
-  * @version V1.0.3
-  * @date    22-April-2016 
-  * @brief   STM32F7xx HAL API Template project 
-  ******************************************************************************
-  * @attention
-  *
+/**
   * <h2><center>&copy; COPYRIGHT(c) 2016 STMicroelectronics</center></h2>
   *
   * Redistribution and use in source and binary forms, with or without modification,
